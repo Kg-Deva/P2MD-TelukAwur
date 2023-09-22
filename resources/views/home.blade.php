35b598c0d0c743a1f639e5296f14a8d7b7a34451
nandhaayu <nandha131102@gmail.com>
2023-09-22 10:43:04 +0700

    create footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,8 +100,34 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-4 col-md-6 footer-contact">
-            <h3><span>Pantai Teluk Awur</span></h3>
+
+        <div class="col-lg-5 col-md-6 footer-links">
+            <!-- <h4>Our Social Networks</h4> -->
+            <img src="assets/img/logopcs.png" alt="">
+            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Link Tautan</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Beranda</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">Profil</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#spot">Spot Pantai</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#warung">Kuliner</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#wisata">Paket Wisata</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#penginapan">Penginapan</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4><span>Kontak Hubung</span></h4>
             <p>
               A108 Adam Street <br>
               New York, NY 535022<br>
@@ -109,17 +135,6 @@
               <strong>Phone:</strong> +1 5589 55488 55<br>
               <strong>Email:</strong> info@example.com<br>
             </p>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
           </div>
 
           <!-- <div class="col-lg-3 col-md-6 footer-links">
@@ -133,17 +148,7 @@
             </ul>
           </div> -->
 
-          <div class="col-lg-4 col-md-6 footer-links">
-            <h4>Our Social Networks</h4>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
-          </div>
+          
 
         </div>
       </div>

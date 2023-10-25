a0cc2b3b3921ee7c6359892f43cd5fa4e7f5a909
nandhaayu <nandha131102@gmail.com>
2023-10-25 10:34:29 +0700

    editing link video

--- resources/views/partials/video.blade.php
+++ resources/views/partials/video.blade.php
@@ -1,20 +1,27 @@
-  <!-- ======= VIDEO Section ======= -->
-  <section class="video">
-      <div class="container" data-aos="fade-up">
-
-        <div class="row" data-aos="fade-up" data-aos-delay="100">
-        @foreach ($footer as $video)
-          <div class="col-lg-6 col-md-6">
-            <div class="info-box mb-4">
-            <iframe width="520" height="315" src="{{ $video->youtube }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              <h3>{{ $video->judul }}</h3>
+        <!-- Penginapan Start -->
+        <section class="video">
+        <div class="container" data-aos="fade-up">
+            <div class="row g-4 justify-content-center">
+            @foreach ($footer as $video)
+                    <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="package-item">
+                            <div class="overflow-hidden">
+                            <iframe width="560" height="315" src="{{ $video->youtube }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                            <div class="d-flex border-bottom">
+                                <p class="flex-fill text-center border-end py-2 ">
+                                  {{ $video->judul }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-          </div>
-          @endforeach
-        </div>
 
-      </div>
-    </section><!-- End VIDEO Section -->
+        </div>
+    </section>
+
+    <!-- Penginapan End -->
+
 
 
 

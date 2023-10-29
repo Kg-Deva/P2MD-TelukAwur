1191156daa47c45dd6dd96222cb418a67cb1d253
nandhaayu <nandha131102@gmail.com>
2023-10-29 19:32:06 +0700

    editing navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,6 +51,7 @@
   <script src="assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
   <script src="assets/js/main1.js"></script>
   <script src="assets/js/main2.js"></script>
 

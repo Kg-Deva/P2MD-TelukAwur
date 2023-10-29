9339966d2b909846aa1b61ec0cf08d92ab916a65
nandhaayu <nandha131102@gmail.com>
2023-10-29 20:24:27 +0700

    responsive video

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -34,7 +34,7 @@
             <h4><span>Kontak Hubung</span></h4>
             <p>
             Kec. Tahunan, Kab. Jepara, Jawa Tengah<br>
-              <strong>Phone:</strong>+62 813-2656-6592<br>
+              <strong>Phone:</strong><a href="https://wa.me/+6281326566592">+62 813-2656-6592</a><br>
               <strong>Email:</strong>bumdestelukmakmur.telukawur@gmail.com<br>
             </p>
           </div>
@@ -46,14 +46,14 @@
 
     <div class="container py-4">
       <div class="copyright">
-        &copy; Copyright <strong><span>Polibang Creative Studio</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span><a href="https://polibangcreativestudio.my.id/">Polibang Creative Studio</a></span></strong>. All Rights Reserved
       </div>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="https://polibangcreativestudio.my.id/">TimP2mdPolibangcreativestudio</a>
       </div>
     </div>
 </div>
--- resources/views/partials/video.blade.php
+++ resources/views/partials/video.blade.php
@@ -6,7 +6,7 @@
                     <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="package-item">
                             <div class="overflow-hidden">
-                            <iframe width="560" height="315" src="{{ $video->youtube }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <iframe class="responsive-iframe" width="560" height="315" src="{{ $video->youtube }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             </div>
                             <div class="d-flex border-bottom">
                                 <p class="flex-fill text-center border-end py-2 ">

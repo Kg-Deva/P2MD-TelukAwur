de8b3784825aa1effe08e6556728f094240027cc
nandhaayu <nandha131102@gmail.com>
2023-10-29 23:49:52 +0700

    update link

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -6,27 +6,27 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/LOGO-PANTAI.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="{{ assets/img/LOGO-PANTAI.png }}" rel="icon">
+  <link href="{{ assets/img/apple-touch-icon.png }}" rel="apple-touch-icon">
 
   <!-- Icon Font Stylesheet -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
   <!-- bootstrap css -->
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ assets/css/bootstrap.min.css }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="{{ assets/vendor/aos/aos.css }}" rel="stylesheet">
+  <link href="{{ assets/vendor/bootstrap/css/bootstrap.min.css }}" rel="stylesheet">
+  <link href="{{ assets/vendor/bootstrap-icons/bootstrap-icons.css }}" rel="stylesheet">
+  <link href="{{ assets/vendor/boxicons/css/boxicons.min.css }}" rel="stylesheet">
+  <link href="{{ assets/vendor/glightbox/css/glightbox.min.css }}" rel="stylesheet">
+  <link href="{{ assets/vendor/swiper/swiper-bundle.min.css }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="{{ assets/css/style.css }}" rel="stylesheet">
